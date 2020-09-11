bb640d1286214fcd97fddcf439202f93cf033749
jValdez <jdv_rodriguez@hotmail.com>
2020-09-11 12:14:10 -0500

    Estructura y componentes
    Instalacion de @angular/cdk y creacion del componente virtual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './virtual/virtual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/virtual/virtual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual',
+  templateUrl: './virtual.component.html',
+  styleUrls: ['./virtual.component.css']
+})
+export class VirtualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
